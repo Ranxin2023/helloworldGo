701ff22e23e713a11be7fb12e4d8e21f4c83a233
Ranxin <rstli@ucdavis.edu>
2023-04-03 17:49:12 -0700

    add more variables

--- helloworld.go
+++ helloworld.go
@@ -1,6 +1,16 @@
 package main
-import ("fmt")
+
+import (
+	"fmt"
+)
 
 func main() {
-  fmt.Println("Hello World!")
+	fmt.Println("Hello World!")
+	var x = 3
+	fmt.Println(x)
+	var arr = [3]int{2, 3, 4}
+	fmt.Println(arr)
+	for i := 0; i < 3; i++ {
+		fmt.Print(arr[i], " ")
+	}
 }
